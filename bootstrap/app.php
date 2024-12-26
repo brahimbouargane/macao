3cb882898d72ebafc219abf3e448a9f29b15993f
Brahim Bouargane <brahim.bouargane2000@gmail.com>
2024-12-26 15:00:28 +0100

    fixed errosPage

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Data\AuthenticatedUserData;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,10 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\SetLocale::class,
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+    $middleware->web(append: [ \App\Http\Middleware\HandleInertiaRequests::class,
+        \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        \App\Http\Middleware\SetLocale::class,
         ]);
 
         //
@@ -26,8 +27,21 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
 
-            if (app()->environment() == "production" && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-                return Inertia::render('errors/errorPage', ['status' => $response->getStatusCode()])
+
+        if (app()->environment() == "local" && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            return Inertia::render(
+                'errors/errorPage',
+                array_merge(
+                    [
+                        'status' => $response->getStatusCode(),
+                        'translations' => function () {
+                            return translations(
+                                resource_path('lang/' . app()->getLocale() . '.json')
+                            );
+                        },
+                    ],
+                )
+            )
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
             } elseif ($response->getStatusCode() === 419) {
@@ -37,6 +51,6 @@ return Application::configure(basePath: dirname(__DIR__))
             }
 
             return $response;
-        });
+    });
     })
     ->create();
