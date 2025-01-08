62e920fbde8a89bf1321c044c1a9ebe29c93199f
Brahim Bouargane <brahim.bouargane2000@gmail.com>
2025-01-08 11:39:25 +0100

    fixed table footer for mobile

--- resources/js/pages/Dashboard/categories/partials/table/data-table-pagination.tsx
+++ resources/js/pages/Dashboard/categories/partials/table/data-table-pagination.tsx
@@ -42,7 +42,7 @@ export function DataTablePagination<TData>({ table, paginatedData }: DataTablePa
           </span>
         </div>
 
-        <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-6 lg:space-x-8 flex-col md:flex-row space-y-3">
           {/* Lines per page */}
           <div className="flex items-center space-x-8">
             <Select
