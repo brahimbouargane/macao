b12958016b11b04f747ea34018e36a911d3b2e65
brahimbouargane <brahim.bouargane2000@gmail.com>
2025-04-10 16:57:55 +0100

    add  other blogs with image on public

--- resources/js/pages/blog/blog-show.tsx
+++ resources/js/pages/blog/blog-show.tsx
@@ -83,7 +83,7 @@ function BlogShow({ post, relatedPosts }) {
 
       <div className="rounded-lg overflow-hidden mb-10">
         <img
-          src={blogDefault}
+          src={post.image || blogDefault}
           alt={post.title}
           width={800}
           height={400}
@@ -181,7 +181,7 @@ function BlogShow({ post, relatedPosts }) {
             <div className="rounded-[40px] overflow-hidden border-2 border-red-700 hover:shadow-lg transition-shadow duration-300 h-full flex flex-col">
               <div className="relative h-64 overflow-hidden">
                 <img
-                  src={blogDefault}
+                  src={post.image || blogDefault}
                   alt={relatedPost.title}
                   className="w-full h-full object-cover transition-transform duration-500 hover:scale-105"
                 />
